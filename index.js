b5bbef00b67025e49a9b5a3806cc3743326fa0a5
vikas <vikas80046@gmail.com>
2024-01-30 14:10:50 +0530

    making some changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ dotenv.config();
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("hello backend");
+  res.send("hello backend,this is for testing purpose only");
 });
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
